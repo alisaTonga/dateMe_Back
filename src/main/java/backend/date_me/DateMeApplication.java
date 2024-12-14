5c21e9964e0d025afdf98eb926bf43295956e713
alisaTonga <tongalise@gmail.com>
2024-12-15 00:28:51 +0100

    created project

---
+++ src/main/java/backend/date_me/DateMeApplication.java
@@ -0,0 +1,13 @@
+package backend.date_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DateMeApplication.class, args);
+    }
+
+}
